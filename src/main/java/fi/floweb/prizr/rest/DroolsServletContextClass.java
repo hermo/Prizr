f496e4aeccf9c552d794d5f17d499983aa266eab
Juha Kervinen <joker@iki.fi>
2015-03-04 13:05:45 +0200

    Init db-based rules on startup

--- src/main/java/fi/floweb/prizr/rest/DroolsServletContextClass.java
+++ src/main/java/fi/floweb/prizr/rest/DroolsServletContextClass.java
@@ -1,5 +1,7 @@
 package fi.floweb.prizr.rest;
 
+import java.util.ArrayList;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -8,11 +10,8 @@ import javax.ws.rs.core.Context;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.FactHandle;
 
 import fi.floweb.prizr.beans.MultiplierBase;
-import fi.floweb.prizr.beans.PricingRequest;
-import fi.floweb.prizr.beans.PricingResponse;
 import fi.floweb.prizr.facts.FactStorage;
 import fi.floweb.prizr.facts.FactStorageMongoDBImpl;
 
@@ -59,34 +58,13 @@ public class DroolsServletContextClass implements ServletContextListener {
 	    KieContainer kContainer = ks.getKieClasspathContainer();
     	DroolsServletContextClass.kSession = kContainer.newKieSession("ksession-rules");
 
-    	// fact
-    	MultiplierBase base = new MultiplierBase();
-    	base.setAppliesToCategory("testCategory");
-    	base.setAppliesToLocation("testplace");
-    	base.setMultiplier(2.5f);
-    	
-    	MultiplierBase baseAnother = new MultiplierBase();
-    	baseAnother.setAppliesToCategory("someOtherCategory");
-    	baseAnother.setAppliesToLocation("testplace");
-    	baseAnother.setMultiplier(100f);
-    	
-        // go !
-        PricingRequest preq = new PricingRequest();
-        preq.setArticleNum("testArticleNum");
-        preq.setArticleCategory("testCategory");
-        preq.setCountry("testCountry");
-        preq.setPlaceCode("testplace");
-        preq.setPurchasePrice(10);
-        
-        
-        PricingResponse pres = new PricingResponse();
-        kSession.insert(preq);
-        FactHandle handle = kSession.insert(pres); 
-        kSession.insert(base);
-        kSession.fireAllRules();
-        PricingResponse response = (PricingResponse) kSession.getObject(handle);
-        System.out.println("Responded with PricingResponse:");
-        System.out.println(response);
+    	// load rules from DB and insert to store
+    	FactStorage storage = new FactStorageMongoDBImpl();
+  	  	ArrayList<MultiplierBase> rules = storage.getFacts();
+    	for(MultiplierBase rule : rules) {
+    		System.out.println("Insert persisted rule: "+rule.getMultiplierBaseName());
+    		kSession.insert(rule);
+    	}
 	}
 
 }
