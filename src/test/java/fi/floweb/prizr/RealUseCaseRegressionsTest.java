e41b4af1514c73ad17867658d37e2fb6ba8294be
Juha Kervinen <joker@iki.fi>
2015-12-05 10:24:03 +0200

    Add new real use case regression test

--- src/test/java/fi/floweb/prizr/RealUseCaseRegressionsTest.java
+++ src/test/java/fi/floweb/prizr/RealUseCaseRegressionsTest.java
@@ -209,4 +209,52 @@ public class RealUseCaseRegressionsTest {
 		assertEquals(response.getPrice(), 1.1590854545454545, DELTA);
 	}
 	
+	@Test
+	public void testHKTDoesNotMatchSKTInPricing() {
+		PricingRequest req1 = new PricingRequest();
+		req1.setCommission(8.00);
+		req1.setCountryCode("NL");
+		req1.setItemCategoryCode("01LEIK-K");	 
+		req1.setItemStandardCost(0.37143);	
+		req1.setItemUnitCC(350);
+		req1.setLocationCode("SKT");
+		req1.setSalesPrice(100.00);
+		req1.setShopCode("SKT");
+		
+		MultiplierBase rule1 = new MultiplierBase();		
+		rule1.setAppliesToCategory("20RU-K");
+		rule1.setAppliesToLocation("HKT");
+		rule1.setMultiplier(2);
+		rule1.setMultiplierBaseDescription("hkt ruk-k ulk");
+		rule1.setMultiplierBaseName("hkt ruk-k");
+		rule1.setAppliesToShopCode("Kaikki");
+		rule1.setDomestic(false);
+		rule1.setCountryCode("-");
+		rule1.setIncludesFreight(20);
+		rule1.setFreightMultiplier(1.2);
+		rule1.setBaseFreightMultiplier(1.2);
+		
+		MultiplierBase rule2 = new MultiplierBase();
+		rule2.setAppliesToCategory("01LEIK-K");
+		rule2.setAppliesToLocation("HKT");
+		rule2.setMultiplier(2);
+		rule2.setMultiplierBaseDescription("hkt leik-k ulk");
+		rule2.setMultiplierBaseName("hkt leik-k");
+		rule2.setAppliesToShopCode("Kaikki");
+		rule2.setDomestic(false);
+		rule2.setCountryCode("-");
+		rule2.setIncludesFreight(20);
+		rule2.setFreightMultiplier(1.2);
+		rule2.setBaseFreightMultiplier(1.2);
+		
+		kSession.insert(rule1);
+		kSession.insert(rule2);
+		kSession.insert(req1); 
+        PricingResponse pres = new PricingResponse();
+        FactHandle handle = kSession.insert(pres);
+        kSession.fireAllRules();
+        PricingResponse response = (PricingResponse) kSession.getObject(handle);
+		assertFalse(response.isPricingDone());
+	}
+	
 }
